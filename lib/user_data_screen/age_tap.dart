152091476ef9ea5bab4a94c4b0b15f3627371e09
Kerolos129Yousef <kerolos.yousef129@gmail.com>
2023-03-07 00:00:19 +0200

    add gender_tap and empty{age_tab,country_tab,eye_image_tap}

---
+++ lib/user_data_screen/age_tap.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AgeTap extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.blue,
+      child: Center(child: Text("Age")),
+    );
+  }
+}
